45a43ad2502136df20397e82cf5fac45e6bc0975
Hasan Malik <hasanimranmalik245@gmail.com>
2025-06-21 01:30:40 -0400

    committing the react frontend
    react frontend commit

---
+++ Frontend/src/header.jsx
@@ -0,0 +1,13 @@
+
+export default function() {
+    return(
+        <>
+                    <header>
+                        {/* <div id="div-header"> */}
+                            <h1>Fit Selector</h1>
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi8FPhqYOMcgQsVR-Os-W0CQLQNJAbbR8N6A&s" alt="Wardrobe" />
+                        {/* </div> */}
+                    </header>
+        </>
+    )
+}
