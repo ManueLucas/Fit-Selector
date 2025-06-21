4e9f04e95d723548b448e66bfa4ab7186cd98323
Sam <tech@imsam.ca>
2025-06-21 11:26:46 -0400

    add clerk header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,20 @@ import "./index.css";
 import Header from "./header";
 import Options from "./options";
 
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+
+
 function App() {
     return (
         <>
+            <header>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </header>
             <h1 id="h1-name">Fit Selector</h1>
             <h2 id="h2-slogan">your fit. your way.</h2>
 
