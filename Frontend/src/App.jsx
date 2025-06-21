8f88e25abac7123734981cc33efd07e408328d0d
Hasan Malik <hasanimranmalik245@gmail.com>
2025-06-21 11:12:41 -0400

    new frontend black and white
    black and white frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,44 +1,31 @@
-import { useState } from 'react'
-import './App.css'
-import Header from "./header"
-import Options from "./options"
+import { useState } from "react";
+import "./index.css";
+import Header from "./header";
+import Options from "./options";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <h1 id="h1-name">Fit Selector</h1>
+            <h2 id="h2-slogan">your fit. your way.</h2>
 
-  return (
-    <>
-      <Header />
-
-      <main>
-        <Options />
-      </main>
-    </>
-  )
+            <h1 id="h1-question">
+                What kind of fit do you want to wear today?
+            </h1>
+            <div id="div-input">
+                <input type="text" placeholder="e.g. I want to look like Chris Hemsworth!" name="" id="input-fit" />
+            </div>
+            <div id="div-input-file">
+                <input type="file" id="input-file" />
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
 
+// <Header />
 
-
-
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+// <main>
+//   <Options />
+// </main>
