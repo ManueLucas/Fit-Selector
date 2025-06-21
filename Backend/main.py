92b3474eaf49577ca23ae17a300000a042f5945c
ManueLucas <m.lucas2603@gmail.com>
2025-06-21 03:41:25 -0400

    added env file for api key stuff and simple gemini embed call

--- Backend/main.py
+++ Backend/main.py
@@ -1,15 +1,36 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi import File, UploadFile, Form
+from google import genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
 
 app = FastAPI()
-
+client = genai.Client(api_key=api_key)  # ← pass the variable here
 
 @app.post("/api/add_image")
-def add_image():
-    return {"Hello": "World"}
+def add_image(file: UploadFile = File(...)):
+    return {"filename": file.filename, "content_type": file.content_type}
 
 
 @app.get("/api/{image_id}")
 def get_image(image_id: str):
     return {"image_id": image_id}
+
+@app.post("/api/query")
+async def query(text: str = Form(...), file: UploadFile = File(...)):
+    print(f'api key: {api_key}')
+    result = client.models.embed_content(
+        model="gemini-embedding-exp-03-07",
+        contents=text)
+
+    return {
+        "question": text,
+        "answer": result,
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "message" : "Thanks for playing!"
+    }
